6837e08a8c2623c882d3796dd2f86518f2329fec
Takuya Horimatsu <new_woory@hotmail.com>
2017-03-07 20:25:00 +0900

    comment / text variable

--- php-codes/test1.php
+++ php-codes/test1.php
@@ -7,8 +7,11 @@ class TestClass1 {
     var $text = "abcdefg";
     var $text2 = "ABC
 DEFG";
+    var $text3 = "<html><body bgcolor=\"white;\"></html>";
+    var $text4 = '<html><body bgcolor=\'white;\'></html>';
 
     function add() {
+        // TODO １行コメント
         return $this->val + 97;
     }
     function minus() {
@@ -20,6 +23,15 @@ DEFG";
     function text() {
         return $this->text;
     }
+    function text2() {
+        return $this->text2;
+    }
+    function text3() {
+        return $this->text3;
+    }
+    function text4() {
+        return $this->text4;
+    }
 }
 $a =& new TestClass1();
 echo $a->val.PHP_EOL;
@@ -27,4 +39,7 @@ echo $a->add().PHP_EOL;
 echo $a->minus().PHP_EOL;
 echo $a->my().PHP_EOL;
 echo $a->text().PHP_EOL;
+echo $a->text2().PHP_EOL;
+echo $a->text3().PHP_EOL;
+echo $a->text4().PHP_EOL;
 ?>
